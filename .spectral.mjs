8506a53de91155b58117229a3551f193229796e3
contato@lpquevende.com.br <contato@lpquevende.com.br>
2023-02-01 11:18:48 -0300

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjU3OTg0/spectral.js?branch_slug=main&token=60b53876-268e-45fd-8407-4d6f7b362c28";
+export default { extends: ruleset };
